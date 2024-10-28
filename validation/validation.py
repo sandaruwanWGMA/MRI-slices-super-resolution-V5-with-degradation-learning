99058b3bbd6de2d8c6b97af0c7a497b37e75e335
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-29 00:57:03 +0530

    x

--- validation/validation.py
+++ validation/validation.py
@@ -67,6 +67,8 @@ for i, data in enumerate(val_loader, 0):
             # Convert tensors to numpy for skimage (run on CPU)
             output_np = output.cpu().numpy()
             high_res_np = high_res_image.cpu().numpy()
+            print("output_np shape:", output_np.shape)
+            print("high_res_np shape:", high_res_np.shape)
 
             # Calculate PSNR and SSIM
             psnr = metrics.peak_signal_noise_ratio(
